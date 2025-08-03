d56e96c1d90a0a88febc94ffe1d4e73f3ae09d87
HotCheesle <ssho1204@naver.com>
2025-08-03 23:29:37 +0900

    fibo

--- Step_up_2579.py
+++ Step_up_2579.py
@@ -3,12 +3,15 @@ stairs = []
 for _ in range(n): 
     stairs.append(int(input()))
 
-step = [stairs[n-1] + stairs[n-2] for _ in range(n+1)]
+step = [0 for _ in range(n)]
 
 def best_step(idx): 
-    step[idx] = max(stairs[idx+1]+step[idx+3], stairs[idx]+step[idx+2])
+    step[idx] = max(stairs[idx] + step[idx+2], stairs[idx] + stairs[idx+1] + step[idx+3])
 
-for i in range(n-3, -1, -1): 
+step[n-1] = stairs[n-1]
+step[n-2] = stairs[n-1] + stairs[n-2]
+step[n-3] = max(stairs[n-1] + stairs[n-2], stairs[n-1] + stairs[n-3])
+for i in range(n-4, -1, -1): 
     best_step(i)
 
 print(step[0])
