d329d1dbc99fbf1f15537f9ebdb0d4cd5050790f
HotCheesle <ssho1204@naver.com>
2025-08-18 22:03:14 +0900

    color paper solv?

--- ColorPaper_10163.py
+++ ColorPaper_10163.py
@@ -1,9 +1,18 @@
 import sys
 
 n = int(sys.stdin.readline())
-papers = [[0]*n]
+plain = list([0]*1001 for _ in range(1001))
 area = [0]*n
+hide = [0]*n
 
+for i in range(1, n+1): 
+    paper = list(map(int, sys.stdin.readline().split()))
+    area[i-1] = paper[2] * paper[3]
+    for j in range(paper[1], paper[1] + paper[3]): 
+        for k in range(paper[0], paper[0] + paper[2]): 
+            if plain[j][k] != 0: 
+                hide[plain[j][k] - 1] += 1
+            plain[j][k] = i
+            
 for i in range(n): 
-    papers[i] = list(map(int, sys.stdin.readline().split()))
-
+    print(area[i] - hide[i])
