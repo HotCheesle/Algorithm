753ef5ef4c3209eae98dc753c22433593b5965a6
HotCheesle <ssho1204@naver.com>
2025-08-21 23:29:07 +0900

    cube solv

--- Baekjoon/Geometry/Cubing_5373.py
+++ Baekjoon/Geometry/Cubing_5373.py
@@ -23,14 +23,14 @@ class Cube:
     def front_turn(self, direction): 
         if direction == '+': 
             self.front.rotate(2)
-            self.up[6], self.right[0], self.down[2], self.left[4] = self.right[0], self.down[2], self.left[4], self.up[6]
-            self.up[5], self.right[7], self.down[1], self.left[3] = self.right[7], self.down[1], self.left[3], self.up[5]
-            self.up[4], self.right[6], self.down[0], self.left[2] = self.right[6], self.down[0], self.left[2], self.up[4]
-        else: 
-            self.front.rotate(-2)
             self.up[6], self.right[0], self.down[2], self.left[4] = self.left[4], self.up[6], self.right[0], self.down[2]
             self.up[5], self.right[7], self.down[1], self.left[3] = self.left[3], self.up[5], self.right[7], self.down[1]
             self.up[4], self.right[6], self.down[0], self.left[2] = self.left[2], self.up[4], self.right[6], self.down[0]
+        else: 
+            self.front.rotate(-2)
+            self.up[6], self.right[0], self.down[2], self.left[4] = self.right[0], self.down[2], self.left[4], self.up[6]
+            self.up[5], self.right[7], self.down[1], self.left[3] = self.right[7], self.down[1], self.left[3], self.up[5]
+            self.up[4], self.right[6], self.down[0], self.left[2] = self.right[6], self.down[0], self.left[2], self.up[4]
     def back_turn(self, direction): 
         if direction == '+': 
             self.back.rotate(2)
@@ -38,9 +38,70 @@ class Cube:
             self.up[1], self.left[7], self.down[5], self.right[3] = self.right[3], self.up[1], self.left[7], self.down[5]
             self.up[0], self.left[6], self.down[4], self.right[2] = self.right[2], self.up[0], self.left[6], self.down[4]
         else: 
-            
+            self.back.rotate(-2)
+            self.up[2], self.left[0], self.down[6], self.right[4] = self.left[0], self.down[6], self.right[4], self.up[2]
+            self.up[1], self.left[7], self.down[5], self.right[3] = self.left[7], self.down[5], self.right[3], self.up[1]
+            self.up[0], self.left[6], self.down[4], self.right[2] = self.left[6], self.down[4], self.right[2], self.up[0]
+    def left_turn(self, direction): 
+        if direction == '+': 
+            self.left.rotate(2)
+            self.up[0], self.front[0], self.down[0], self.back[4] = self.back[4], self.up[0], self.front[0], self.down[0]
+            self.up[7], self.front[7], self.down[7], self.back[3] = self.back[3], self.up[7], self.front[7], self.down[7]
+            self.up[6], self.front[6], self.down[6], self.back[2] = self.back[2], self.up[6], self.front[6], self.down[6]
+        else: 
+            self.left.rotate(-2)
+            self.up[0], self.front[0], self.down[0], self.back[4] = self.front[0], self.down[0], self.back[4], self.up[0]
+            self.up[7], self.front[7], self.down[7], self.back[3] = self.front[7], self.down[7], self.back[3], self.up[7]
+            self.up[6], self.front[6], self.down[6], self.back[2] = self.front[6], self.down[6], self.back[2], self.up[6]
+    def right_turn(self, direction): 
+        if direction == '+': 
+            self.right.rotate(2)
+            self.up[4], self.back[0], self.down[4], self.front[4] = self.front[4], self.up[4], self.back[0], self.down[4]
+            self.up[3], self.back[7], self.down[3], self.front[3] = self.front[3], self.up[3], self.back[7], self.down[3]
+            self.up[2], self.back[6], self.down[2], self.front[2] = self.front[2], self.up[2], self.back[6], self.down[2]
+        else: 
+            self.right.rotate(-2)
+            self.up[4], self.back[0], self.down[4], self.front[4] = self.back[0], self.down[4], self.front[4], self.up[4]
+            self.up[3], self.back[7], self.down[3], self.front[3] = self.back[7], self.down[3], self.front[3], self.up[3]
+            self.up[2], self.back[6], self.down[2], self.front[2] = self.back[6], self.down[2], self.front[2], self.up[2]
+    def down_turn(self, direction): 
+        if direction == '+': 
+            self.down.rotate(2)
+            self.front[6], self.right[6], self.back[6], self.left[6] = self.left[6], self.front[6], self.right[6], self.back[6]
+            self.front[5], self.right[5], self.back[5], self.left[5] = self.left[5], self.front[5], self.right[5], self.back[5]
+            self.front[4], self.right[4], self.back[4], self.left[4] = self.left[4], self.front[4], self.right[4], self.back[4]
+        else: 
+            self.down.rotate(-2)
+            self.front[6], self.right[6], self.back[6], self.left[6] = self.right[6], self.back[6], self.left[6], self.front[6]
+            self.front[5], self.right[5], self.back[5], self.left[5] = self.right[5], self.back[5], self.left[5], self.front[5]
+            self.front[4], self.right[4], self.back[4], self.left[4] = self.right[4], self.back[4], self.left[4], self.front[4]
+    def show_up(self): 
+        for i in range(3): 
+            print(self.up[i], end='')
+        print()
+        print(self.up[7], end='')
+        print('w', end='')
+        print(self.up[3])
+        for i in range(6, 3, -1): 
+            print(self.up[i], end='')
+        print()
 
-
-cube = Cube()
-cube.up_turn('+')
-print(cube.front)
+T = int(input())
+for tc in range(1, T+1): 
+    N = int(input())
+    cube = Cube()
+    turn_list = list(input().split())
+    for turn in turn_list: 
+        if turn[0] == 'U': 
+            cube.up_turn(turn[1])
+        elif turn[0] == 'D': 
+            cube.down_turn(turn[1])
+        elif turn[0] == 'F': 
+            cube.front_turn(turn[1])
+        elif turn[0] == 'B': 
+            cube.back_turn(turn[1])
+        elif turn[0] == 'L': 
+            cube.left_turn(turn[1])
+        elif turn[0] == 'R': 
+            cube.right_turn(turn[1])
+    cube.show_up()
