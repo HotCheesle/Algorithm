293c93c82e8bd1438d39066c458d4a75a32343b5
HotCheesle <ssho1204@naver.com>
2025-08-26 23:33:58 +0900

    solv knight

--- Baekjoon/knight_move_7562.py
+++ Baekjoon/knight_move_7562.py
@@ -5,21 +5,29 @@ for tc in range(1, T+1):
     I = int(input())
     cur_x, cur_y = map(int, input().split())
     goal_x, goal_y = map(int, input().split())
-    dif_x, dif_y = abs(cur_x - goal_x), abs(cur_y - goal_y)
     cnt = 0
     dr, dc = [-2, -1, 1, 2, 2, 1, -1, -2], [1, 2, 2, 1, -1, -2, -2, -1]
-    queue = deque([])
-    while dif_x + dif_y < 10: 
-        if dif_x > dif_y: 
-            dif_x = abs(dif_x - 2)
-            dif_y = abs(dif_y - 1)
+    
+    queue = deque([(cur_x, cur_y, 0)])
+    visited = {(cur_x, cur_y)}
+    bottom = min(cur_y, goal_y) - 3
+    if bottom < 0: bottom = 0
+    top = max(cur_y, goal_y) + 4
+    if top > I: top = I
+    left = min(cur_x, goal_x) - 3
+    if left < 0: left = 0
+    right = max(cur_x, goal_x) + 4
+    if right > I: right = I
+    while queue: 
+        cur = queue.popleft()
+        if cur[0] == goal_x and cur[1] == goal_y: 
+            cnt = cur[2]
+            break
         else: 
-            dif_x = abs(dif_x - 1)
-            dif_y = abs(dif_y - 2)
-    
-    cur_x, cur_y = 0, 0
-    goal_x, goal_y = dif_x, dif_y
-    queue = deque([])
-    
+            for y, x in zip(dr, dc): 
+                if left <= cur[0]+x < right and bottom <= cur[1]+y < top:
+                    if (cur[0]+x, cur[1]+y) not in visited: 
+                        visited.add((cur[0]+x, cur[1]+y))
+                        queue.append((cur[0]+x, cur[1]+y, cur[2]+1))
 
     print(cnt)
