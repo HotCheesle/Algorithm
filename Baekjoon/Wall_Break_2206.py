159aad4564bded8978cf870a11de4ccc275b0831
이승호 <ssho1204@naver.com>
2025-08-28 17:39:41 +0900

    wall breake solv

--- Baekjoon/Wall_Break_2206.py
+++ Baekjoon/Wall_Break_2206.py
@@ -1,6 +1,4 @@
-from collections import deque
-
-def BFS(st_r, st_c, distance): 
+def BFS(st_r, st_c, distance, dis_map): 
     queue[0] = (st_r, st_c, distance)
     visited.add((st_r, st_c))
     front, rear = 0, 1
@@ -14,35 +12,58 @@ def BFS(st_r, st_c, distance):
                     visited.add((row, col))
                     queue[rear] = (row, col, dis)
                     rear += 1
-                    d_map[row][col] = dis
+                    dis_map[row][col] = dis
     return rear
 
 R, C = map(int, input().split())
 maze = list(list(input()) for _ in range(R))
 d_map = list(list(0 for _ in range(C)) for _ in range(R))
-d_map[0][0] = 100001
-#queue = deque([])
-queue = [(0, 0, 0) for _ in range(50000)]
+d_map[0][0] = 1000001
+queue = [(0, 0, 0) for _ in range(1000001)]
 visited = set()
 dr, dc = [-1, 0, 1, 0], [0, 1, 0, -1]
-dr2, dc2 = [-2, 0, 2, 0], [0, 2, 0, -2]
-
-length = BFS(0, 0, 100001)
+length = BFS(0, 0, 1000001, d_map)
+# 예외1 막힌 길로 뚫었는데 그쪽이 큰 숫자라 shrink가 크게 일어난것으로 착각함 그러면 뚫을때 마다 확인? -> 해결
+# 예외2 코너를 돌아가는것 (3방향)을 탐지해야 함 이전에는 직선만 탐지했었다...
 
 if d_map[R-1][C-1] != 0: 
-    max_shrink = 0
+    rev_d_map = list(list(0 for _ in range(C)) for _ in range(R))
+    rev_d_map[R-1][C-1] = 1000001
+    visited.clear()
+    length = BFS(R-1, C-1, 1000001, rev_d_map)
+    min_dis = d_map[R-1][C-1] % 1000000
     for idx in range(length):
         row, col, distance = queue[idx] 
         for r, c in zip(dr, dc): 
             if 0 <= row+r < R and 0 <= col+c < C and d_map[row+r][col+c] == 0: 
-                if 0 <= row+r*2 < R and 0 <= col+c*2 < C: 
-                    shrink = d_map[row+r*2][col+c*2] - d_map[row][col]
-                    if max_shrink < shrink: 
-                        max_shrink = shrink
-    tot_dis = d_map[R-1][C-1] % 100000 - max_shrink + 2
-    print(tot_dis)
+                w_row, w_col = row+r, col+c
+                for y, x in zip(dr, dc): 
+                    if row == w_row+y and col == w_col+x: # 되돌아가는거 방지
+                        continue
+                    if 0 <= w_row+y < R and 0 <= w_col+x < C and d_map[w_row+y][w_col+x] != 0:
+                        dis = d_map[w_row+y][w_col+x] % 1000000 + distance % 1000000 + 1
+                        if min_dis > dis: 
+                            min_dis = dis
+    print(min_dis)
 else: 
-    d_map[R-1][C-1] = 200001
-    queue.clear()
+    min_dis = None
+    d_map[R-1][C-1] = 2000001
+    queue = [(0, 0, 0) for _ in range(1000001)]
     visited.clear()
-    length = BFS(R-1, C-1, 200001)
+    length = BFS(R-1, C-1, 2000001, d_map)
+    for idx in range(length):
+        row, col, distance = queue[idx] 
+        for r, c in zip(dr, dc): 
+            if 0 <= row+r < R and 0 <= col+c < C and d_map[row+r][col+c] == 0: 
+                w_row, w_col = row+r, col+c
+                for y, x in zip(dr, dc): 
+                    if 0 <= w_row+y < R and 0 <= w_col+x < C and d_map[w_row+y][w_col+x] // 1000000 == 1:
+                        dis = d_map[w_row+y][w_col+x] % 1000000 + distance % 1000000 + 1
+                        if min_dis is None: 
+                            min_dis = dis
+                        elif min_dis > dis: 
+                            min_dis = dis
+    if min_dis: 
+        print(min_dis)
+    else: 
+        print(-1)
