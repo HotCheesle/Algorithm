f283dae2ecf96275d15657593d7e839897d17b65
이승호 <ssho1204@naver.com>
2025-09-08 17:59:57 +0900

    stil solving...

--- Baekjoon/BackTracking/Alphabet_1987.py
+++ Baekjoon/BackTracking/Alphabet_1987.py
@@ -1,22 +1,25 @@
 from heapq import heappush
 
-def DFS_bt(row, col, visited):
+def DFS_bt(row, col, visited, prelock):
     leaf = True 
+    locked = prelock.copy()
     for r, c in zip(dr, dc): 
         nrow, ncol = row+r, col+c
-        if 0 <= nrow < R and 0 <= ncol < C and board[nrow][ncol] not in visited: 
-            visited.add(board[nrow][ncol])
-            leaf = False
-            DFS_bt(nrow, ncol, visited)
-            visited.remove(board[nrow][ncol])
+        if 0 <= nrow < R and 0 <= ncol < C and (nrow, ncol) not in locked: 
+            if board[nrow][ncol] not in visited: 
+                visited.add(board[nrow][ncol])
+                leaf = False
+                DFS_bt(nrow, ncol, visited, locked)
+                visited.remove(board[nrow][ncol])
+        locked.add((nrow, ncol))
     if leaf: 
         heappush(max_heap,-len(visited))
-
 
 R, C = map(int, input().split())
 board = list(input() for _ in range(R))
 visited = set(board[0][0])
+lock = set()
 max_heap = []
 dr, dc = [-1, 0, 1, 0], [0, 1, 0, -1]
-DFS_bt(0, 0, visited)
+DFS_bt(0, 0, visited, lock)
 print(max_heap[0]*(-1))
