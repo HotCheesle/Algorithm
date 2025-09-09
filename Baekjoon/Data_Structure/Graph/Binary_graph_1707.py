df0b2ad368df4d88b64ff5748133f400b3a3f5ff
이승호 <ssho1204@naver.com>
2025-09-09 18:00:05 +0900

    solv?

--- Baekjoon/Data_Structure/Graph/Binary_graph_1707.py
+++ Baekjoon/Data_Structure/Graph/Binary_graph_1707.py
@@ -5,26 +5,33 @@ T = int(input())
 for tc in range(T): 
     V, E = map(int, input().split())
     graph = {i: set() for i in range(1, V+1)}
+    edged_vertex = set()
     for _ in range(E): 
         st, ed = map(int, sys.stdin.readline().split())
         graph[st].add(ed)
         graph[ed].add(st)
-    b_set1, b_set2 = set([1]), set()
-    cnt = 0
+        edged_vertex.add(st)
+        edged_vertex.add(ed)
+    b_set1, b_set2 = set([st]), set()
+    edged_v = len(edged_vertex)
     visited = set()
-    queue = deque([[1]])
-    while len(visited) != V: 
-        for v in queue.popleft(): 
+    queue = deque([([st], 0)])
+    while len(visited) != edged_v: 
+        if not queue: 
+            not_visited = edged_vertex.difference(visited)
+            nst = not_visited.pop()
+            queue.append(([nst], 0))
+        now = queue.popleft()
+        for v in now[0]: 
             if v in visited: continue
-            if cnt % 2 == 0: 
+            if now[1] == 0: 
                 b_set2 = b_set2.union(graph[v])
-                queue.append(graph[v])
                 visited.add(v)
+                queue.append((graph[v], 1))
             else: 
                 b_set1 = b_set1.union(graph[v])
-                queue.append(graph[v])
                 visited.add(v)
-        cnt += 1
+                queue.append((graph[v], 0))
         if b_set1.intersection(b_set2): 
             print('NO')
             break
