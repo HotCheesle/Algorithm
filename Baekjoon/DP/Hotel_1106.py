39d1c057cf18e614ee03a47d8070ffadcc7149b0
이승호 <ssho1204@naver.com>
2025-09-12 18:01:47 +0900

    not solv

--- Baekjoon/DP/Hotel_1106.py
+++ Baekjoon/DP/Hotel_1106.py
@@ -1,14 +1,17 @@
-import math
 C, N = map(int, input().split())
 cytis = []
+max_customer = 0
 for _ in range(N): 
     cost, customer = map(int, input().split())
+    max_customer = max(max_customer, customer)
     cytis.append((cost, customer))
 cost, customer = 0, 0
-while customer < C: 
-    remain = C - customer
-    cost_list = list(map(lambda c: int(math.ceil(remain/c[1]))*c[0], cytis))
-    cheap_city = cost_list.index(min(cost_list))
-    cost += cytis[cheap_city][0]
-    customer += cytis[cheap_city][1]
-print(cost)
+dp = [10**9 for _ in range(C+max_customer)]
+dp[0] = 0
+for c in range(1, C+max_customer): 
+    for cyti in cytis: 
+        ref = c - cyti[1]
+        if ref >= 0: 
+            dp[c] = min(dp[c], dp[ref]+cyti[0])
+min_cost = min(dp[C:])
+print(min_cost)
