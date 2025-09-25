303e9046921f5213498fe9882a75397713366fe3
이승호 <ssho1204@naver.com>
2025-09-25 13:40:15 +0900

    solv pre order print

--- Baekjoon/Div_and_Con/Tree_get_preorder_2263.py
+++ Baekjoon/Div_and_Con/Tree_get_preorder_2263.py
@@ -1,39 +1,22 @@
-def make_tree(p_in, parent, is_right): 
-    if not p_in : return None
-    elif len(p_in) == 1: 
-        if is_right: 
-            nodes[parent][2] = p_in[0]
-            confirmed.add(p_in[0])
-        else: 
-            nodes[parent][1] = p_in[0]
-            confirmed.add(p_in[0])
+def print_pre_order(in_ofs_s, in_ofs_e, post_ofs_s, post_ofs_e, inorder, postorder): 
+    if in_ofs_e < in_ofs_s : return None
+    elif in_ofs_s == in_ofs_e: 
+        print(inorder[in_ofs_s], end=' ')
     else: 
-        root = postorder.pop()
-        while root in confirmed: 
-            root = postorder.pop()
-        confirmed.add(root)
-        if is_right: 
-            nodes[parent][2] = root
-        else: 
-            nodes[parent][1] = root
-        root_idx = p_in.index(root)
-        make_tree(p_in[root_idx+1:], root, True)
-        make_tree(p_in[:root_idx], root, False)
-
-def pre_order(root): 
-    if root == -1: return None
-    print(root, end=' ')
-    pre_order(nodes[root][1])
-    pre_order(nodes[root][2])
+        root = postorder[post_ofs_e]
+        post_ofs_e -= 1
+        print(root, end=' ')
+        root_idx = 0
+        while inorder[in_ofs_s+root_idx] != root: 
+            root_idx += 1
+        print_pre_order(in_ofs_s, in_ofs_s+root_idx-1, post_ofs_s, post_ofs_s+root_idx-1, inorder, postorder)
+        print_pre_order(in_ofs_s+root_idx+1, in_ofs_e, post_ofs_s+root_idx, post_ofs_e, inorder, postorder)
 
 import sys
 sys.stdin = open('Algorithm/Baekjoon/Div_and_Con/Tree_get_preorder_2263_test_input.txt', 'r')
+sys.setrecursionlimit(10000)
 
 N = int(input())
 inorder = list(map(int, input().split()))
 postorder = list(map(int, input().split()))
-confirmed = set()
-nodes = list([i, -1, -1] for i in range(N+1)) # (node, left, right)
-root = postorder[-1]
-make_tree(inorder, 0, True) # root의 부모는 0(없음)
-pre_order(root)
+print_pre_order(0, N-1, 0, N-1, inorder, postorder)
